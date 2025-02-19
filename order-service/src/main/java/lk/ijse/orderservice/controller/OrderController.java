5f228124746f6242ee28461633b145fed48aac9d
Ranindu Nethmina <ranindunethmina@gmail.com>
2025-02-19 18:17:46 +0530

    Initial commit

---
+++ order-service/src/main/java/lk/ijse/orderservice/controller/OrderController.java
@@ -0,0 +1,15 @@
+package lk.ijse.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/all")
+    public String getOrder() {
+        return "Orders: od1, od2";
+    }
+}
